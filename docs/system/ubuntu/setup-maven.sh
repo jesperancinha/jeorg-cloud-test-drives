5191256d6650d57d8ba0bef83808b384f57fcf36
Joao Esperancinha <jofisaes@gmail.com>
2021-07-12 22:23:54 +0200

    📜 Maven update

--- docs/system/ubuntu/setup-maven.sh
+++ docs/system/ubuntu/setup-maven.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
-sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
+sudo tar xf /tmp/apache-maven-3.6.3-bin.tar.gz -C /opt
+sudo unlink /opt/maven
 sudo ln -s /opt/apache-maven-3.6.3 /opt/maven
-echo "export M2_HOME=/opt/maven
-export MAVEN_HOME=/opt/maven" >> /etc/profile.d/maven.sh
+sudo echo "export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}" >> maven.sh
+sudo mv maven.sh /etc/profile.d/
 sudo chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
