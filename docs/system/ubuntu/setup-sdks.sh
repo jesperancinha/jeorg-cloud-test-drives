adae030292820701767270ea6bceb2e6a27f028c
Joao Esperancinha <jofisaes@gmail.com>
2022-09-08 20:09:18 +0200

    jdk 18 as the next JDK

--- docs/system/ubuntu/setup-sdks.sh
+++ docs/system/ubuntu/setup-sdks.sh
@@ -4,6 +4,7 @@ sdk install java 11.0.11.hs-adpt
 sdk install java 12.0.2.hs-adpt
 sdk install java 16.0.1.hs-adpt
 sdk install java 17.0.4.1-zulu
+sdk install java 18.0.2-open
 
 echo "" >> ~/.zshrc
 echo alias jdk8="sdk use java 8.0.302-open" >> ~/.zshrc
@@ -11,4 +12,5 @@ echo alias jdk11="sdk use java 11.0.11.hs-adpt" >> ~/.zshrc
 echo alias jdk12="sdk use java 12.0.2.hs-adpt" >> ~/.zshrc
 echo alias jdk16="sdk use java 16.0.1.hs-adpt" >> ~/.zshrc
 echo alias jdk17="sdk use java 17.0.4.1-zulu" >> ~/.zshrc
+echo alias jdk18="sdk use java 18.0.2-open" >> ~/.zshrc
 
