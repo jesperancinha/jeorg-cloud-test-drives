49586075a1d4550fa5ba5b7174fd69223e46d06e
Joao Esperancinha <jofisaes@gmail.com>
2022-09-08 20:05:20 +0200

    Auto SDK installation script

---
+++ docs/system/ubuntu/setup-sdks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sdk install java 8.0.302-open
+sdk install java 11.0.11.hs-adpt
+sdk install java 12.0.2.hs-adpt
+sdk install java 16.0.1.hs-adpt
+sdk install java 17.0.4.1-zulu
+
+echo "" >> ~/.zshrc
+echo alias jdk8="sdk use java 8.0.302-open" >> ~/.zshrc
+echo alias jdk11="sdk use java 11.0.11.hs-adpt" >> ~/.zshrc
+echo alias jdk12="sdk use java 12.0.2.hs-adpt" >> ~/.zshrc
+echo alias jdk16="sdk use java 16.0.1.hs-adpt" >> ~/.zshrc
+echo alias jdk17="sdk use java 17.0.4.1-zulu" >> ~/.zshrc
+
