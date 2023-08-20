3b71c7467bcfbf9d98712cc1a3e509038ebf43e8
Joao Esperancinha <jofisaes@gmail.com>
2023-08-20 21:11:53 +0200

    sudo apt install

--- docs/system/ubuntu/setup.sh
+++ docs/system/ubuntu/setup.sh
@@ -96,6 +96,7 @@ sudo apt update -y
 sudo apt install brave-browser -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install evdi-dkms -y
 sudo apt upgrade -y
 sudo apt update -y
 sudo apt-get install postgresql-client -y
