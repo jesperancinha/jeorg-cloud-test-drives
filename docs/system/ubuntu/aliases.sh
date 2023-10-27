c88a8da5af1794dcd07701ae69bb15ff1e2d745c
João Filipe Sabino Esperancinha <jofisaes@gmail.com>
2023-10-27 13:58:45 +0200

    Update aliases.sh

--- docs/system/ubuntu/aliases.sh
+++ docs/system/ubuntu/aliases.sh
@@ -186,3 +186,9 @@ alias sound-reset='
         systemctl --user restart pulseaudio.socket; \
         sudo alsa force-reload
 '
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
