84ba92e164cae42d63b3e111d5c6e27c757cf7e4
João Filipe Sabino Esperancinha <jofisaes@gmail.com>
2024-09-09 13:06:53 +0200

    Update commands.sh

--- docs/system/macos/commands.sh
+++ docs/system/macos/commands.sh
@@ -49,6 +49,17 @@ echo "- Update complete!"
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 11.0.11.hs-adpt
-sdk install sbt
+yes | sdk install java 8.0.302-open
+yes | sdk install java 11.0.11.hs-adpt
+yes | sdk install java 12.0.2.hs-adpt
+yes | sdk install java 16.0.1.hs-adpt
+yes | sdk install java 17.0.4.1-zulu
+yes | sdk install java 18.0.2-open
+yes | sdk install java 19.0.2-open
+yes | sdk install java 20.0.1-open
+yes | sdk install java 21.0.1.crac-zulu
+yes | sdk install java 21.0.1-tem
+yes | sdk install sbt
+yes | sdk install gradle
+yes | sdk install kotlin
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
