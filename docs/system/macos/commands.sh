cbdca744d2501eb2c4320571f31f7e23985b630b
João Filipe Sabino Esperancinha <jofisaes@gmail.com>
2024-09-09 13:55:45 +0200

    Update commands.sh

--- docs/system/macos/commands.sh
+++ docs/system/macos/commands.sh
@@ -29,6 +29,7 @@ brew install jq
 brew install zsh
 brew install python
 brew install --cask docker
+brew install locust
 echo "- Dev tools installation complete!"
 
 echo "- Python installations..."
